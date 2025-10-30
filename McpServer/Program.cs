0fe152ce9ffeb44da92128da7d3d20b80bb160f6
Egan Nguyen <>
2025-10-30 16:39:18 +0700

    Update Program.cs

--- McpServer/Program.cs
+++ McpServer/Program.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography.X509Certificates;
 using System.Text.Json;
 using DotNetEnv;
 using McpServer;
@@ -21,8 +22,39 @@ builder.Services.AddDbContext<ApplicationDbContext>(opt => opt.UseNpgsql(builder
 builder.Services.AddHttpClient<IEpicClient, EpicClient>();
 builder.Services.AddHttpClient<IOdooClient, OdooClient>();  
 builder.Services.AddScoped<IPostgresService, PostgresService>();
+
+
+
 var app = builder.Build();
 
-app.MapMcp();
+// minimal api for EPIC verify access token
+var keyId = builder.Configuration.GetValue<string>("EPIC:KID");
+string home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+string pemPath = Path.Combine(home, "publickey509.pem");
+string pem = File.ReadAllText(pemPath);
+var cert = X509Certificate2.CreateFromPem(pem);
 
+// Extract RSA public key
+var rsa = cert.GetRSAPublicKey();
+
+// Export modulus/exponent for JWK
+var parameters = rsa.ExportParameters(false);
+
+string Base64UrlEncode(byte[] input) =>
+    Convert.ToBase64String(input)
+        .TrimEnd('=')
+        .Replace('+', '-')
+        .Replace('/', '_');
+
+var jwk = new
+{
+    kty = "RSA",
+    kid = keyId,
+    use = "sig",
+    alg = "RS384", // must match your signing algorithm
+    n = Base64UrlEncode(parameters.Modulus),
+    e = Base64UrlEncode(parameters.Exponent)
+};
+app.MapGet("/.well-known/jwks.json", () => new { keys = new[] { jwk } });
+app.MapMcp();
 app.Run();
